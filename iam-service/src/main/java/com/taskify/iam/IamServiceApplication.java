b736ecf24d67b1dca13ff0a732e75232b9231544
Tondeptrai23 <22120375@student.hcmus.edu.vn>
2025-02-27 15:43:44 +0700

    feat(common): add health check for services

--- iam-service/src/main/java/com/taskify/iam/IamServiceApplication.java
+++ iam-service/src/main/java/com/taskify/iam/IamServiceApplication.java
@@ -2,13 +2,14 @@ package com.taskify.iam;
 
 import com.taskify.common.CommonLibApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 @Import(CommonLibApplication.class)
 @EnableDiscoveryClient
 public class IamServiceApplication {
--- organization-service/src/main/java/com/taskify/organization/OrganizationServiceApplication.java
+++ organization-service/src/main/java/com/taskify/organization/OrganizationServiceApplication.java
@@ -2,12 +2,13 @@ package com.taskify.organization;
 
 import com.taskify.common.CommonLibApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 @Import(CommonLibApplication.class)
 @EnableDiscoveryClient
 public class OrganizationServiceApplication {
