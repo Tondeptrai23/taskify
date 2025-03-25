3c52fc100a202d2708cf60ec52107e03b949e160
Luu Thai Toan <22120375@student.hcmus.edu.vn>
2025-03-25 13:22:31 +0700

    fix: add default values for event constants

--- libraries/common-lib-core/src/main/java/com/taskify/commoncore/event/EventConstants.java
+++ libraries/common-lib-core/src/main/java/com/taskify/commoncore/event/EventConstants.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+/**
+ * Centralized constants for RabbitMQ event names.
+ * All properties have default values but can be overridden through configuration.
+ */
 @Getter
 @Component
 public class EventConstants {
@@ -65,57 +69,56 @@ public class EventConstants {
     @Autowired
     public EventConstants(
             // Exchange names
-            @Value("${rabbitmq.exchange.user-events}") String userEventsExchange,
-            @Value("${rabbitmq.exchange.membership-events}") String membershipEventsExchange,
-            @Value("${rabbitmq.exchange.organization-events}") String organizationEventsExchange,
-            @Value("${rabbitmq.exchange.project-events}") String projectEventsExchange,
-            @Value("${rabbitmq.exchange.project-membership-events}") String projectMembershipEventsExchange,
+            @Value("${rabbitmq.exchange.user-events:taskify.user.events}") String userEventsExchange,
+            @Value("${rabbitmq.exchange.membership-events:taskify.membership.events}") String membershipEventsExchange,
+            @Value("${rabbitmq.exchange.organization-events:taskify.organization.events}") String organizationEventsExchange,
+            @Value("${rabbitmq.exchange.project-events:taskify.project.events}") String projectEventsExchange,
+            @Value("${rabbitmq.exchange.project-membership-events:taskify.project-membership.events}") String projectMembershipEventsExchange,
 
             // Routing keys - User events
-            @Value("${rabbitmq.routing-key.user-created}") String userCreatedRoutingKey,
-            @Value("${rabbitmq.routing-key.user-deleted}") String userDeletedRoutingKey,
+            @Value("${rabbitmq.routing-key.user-created:user.created}") String userCreatedRoutingKey,
+            @Value("${rabbitmq.routing-key.user-deleted:user.deleted}") String userDeletedRoutingKey,
 
             // Routing keys - Organization events
-            @Value("${rabbitmq.routing-key.membership-added}") String membershipAddedRoutingKey,
-            @Value("${rabbitmq.routing-key.membership-removed}") String membershipRemovedRoutingKey,
-            @Value("${rabbitmq.routing-key.membership-role-updated}") String membershipRoleUpdatedRoutingKey,
-            @Value("${rabbitmq.routing-key.organization-created}") String organizationCreatedRoutingKey,
-            @Value("${rabbitmq.routing-key.organization-updated}") String organizationUpdatedRoutingKey,
-            @Value("${rabbitmq.routing-key.organization-deleted}") String organizationDeletedRoutingKey,
+            @Value("${rabbitmq.routing-key.membership-added:membership.added}") String membershipAddedRoutingKey,
+            @Value("${rabbitmq.routing-key.membership-removed:membership.removed}") String membershipRemovedRoutingKey,
+            @Value("${rabbitmq.routing-key.membership-role-updated:membership.role.updated}") String membershipRoleUpdatedRoutingKey,
+            @Value("${rabbitmq.routing-key.organization-created:organization.created}") String organizationCreatedRoutingKey,
+            @Value("${rabbitmq.routing-key.organization-updated:organization.updated}") String organizationUpdatedRoutingKey,
+            @Value("${rabbitmq.routing-key.organization-deleted:organization.deleted}") String organizationDeletedRoutingKey,
 
             // Routing keys - Project events
-            @Value("${rabbitmq.routing-key.project-created}") String projectCreatedRoutingKey,
-            @Value("${rabbitmq.routing-key.project-updated}") String projectUpdatedRoutingKey,
-            @Value("${rabbitmq.routing-key.project-deleted}") String projectDeletedRoutingKey,
-            @Value("${rabbitmq.routing-key.project-member-added}") String projectMemberAddedRoutingKey,
-            @Value("${rabbitmq.routing-key.project-member-removed}") String projectMemberRemovedRoutingKey,
-            @Value("${rabbitmq.routing-key.project-member-role-updated}") String projectMemberRoleUpdatedRoutingKey,
+            @Value("${rabbitmq.routing-key.project-created:project.created}") String projectCreatedRoutingKey,
+            @Value("${rabbitmq.routing-key.project-updated:project.updated}") String projectUpdatedRoutingKey,
+            @Value("${rabbitmq.routing-key.project-deleted:project.deleted}") String projectDeletedRoutingKey,
+            @Value("${rabbitmq.routing-key.project-member-added:project.member.added}") String projectMemberAddedRoutingKey,
+            @Value("${rabbitmq.routing-key.project-member-removed:project.member.removed}") String projectMemberRemovedRoutingKey,
+            @Value("${rabbitmq.routing-key.project-member-role-updated:project.member.role.updated}") String projectMemberRoleUpdatedRoutingKey,
 
-            // IAM service queues
-            @Value("${rabbitmq.queue.iam-user-created}") String iamUserCreatedQueue,
-            @Value("${rabbitmq.queue.iam-user-deleted}") String iamUserDeletedQueue,
-            @Value("${rabbitmq.queue.iam-membership-added}") String iamMembershipAddedQueue,
-            @Value("${rabbitmq.queue.iam-membership-removed}") String iamMembershipRemovedQueue,
-            @Value("${rabbitmq.queue.iam-membership-role-updated}") String iamMembershipRoleUpdatedQueue,
-            @Value("${rabbitmq.queue.iam-organization-created}") String iamOrganizationCreatedQueue,
-            @Value("${rabbitmq.queue.iam-organization-updated}") String iamOrganizationUpdatedQueue,
-            @Value("${rabbitmq.queue.iam-organization-deleted}") String iamOrganizationDeletedQueue,
-            @Value("${rabbitmq.queue.iam-project-created}") String iamProjectCreatedQueue,
-            @Value("${rabbitmq.queue.iam-project-updated}") String iamProjectUpdatedQueue,
-            @Value("${rabbitmq.queue.iam-project-deleted}") String iamProjectDeletedQueue,
-            @Value("${rabbitmq.queue.iam-project-member-added}") String iamProjectMemberAddedQueue,
-            @Value("${rabbitmq.queue.iam-project-member-removed}") String iamProjectMemberRemovedQueue,
-            @Value("${rabbitmq.queue.iam-project-member-role-updated}") String iamProjectMemberRoleUpdatedQueue,
+            // Queue names - IAM service
+            @Value("${rabbitmq.queue.iam-user-created:taskify.iam.user.created}") String iamUserCreatedQueue,
+            @Value("${rabbitmq.queue.iam-user-deleted:taskify.iam.user.deleted}") String iamUserDeletedQueue,
+            @Value("${rabbitmq.queue.iam-membership-added:taskify.iam.membership.added}") String iamMembershipAddedQueue,
+            @Value("${rabbitmq.queue.iam-membership-removed:taskify.iam.membership.removed}") String iamMembershipRemovedQueue,
+            @Value("${rabbitmq.queue.iam-membership-role-updated:taskify.iam.membership.role.updated}") String iamMembershipRoleUpdatedQueue,
+            @Value("${rabbitmq.queue.iam-organization-created:taskify.iam.organization.created}") String iamOrganizationCreatedQueue,
+            @Value("${rabbitmq.queue.iam-organization-updated:taskify.iam.organization.updated}") String iamOrganizationUpdatedQueue,
+            @Value("${rabbitmq.queue.iam-organization-deleted:taskify.iam.organization.deleted}") String iamOrganizationDeletedQueue,
+            @Value("${rabbitmq.queue.iam-project-created:taskify.iam.project.created}") String iamProjectCreatedQueue,
+            @Value("${rabbitmq.queue.iam-project-updated:taskify.iam.project.updated}") String iamProjectUpdatedQueue,
+            @Value("${rabbitmq.queue.iam-project-deleted:taskify.iam.project.deleted}") String iamProjectDeletedQueue,
+            @Value("${rabbitmq.queue.iam-project-member-added:taskify.iam.project.member.added}") String iamProjectMemberAddedQueue,
+            @Value("${rabbitmq.queue.iam-project-member-removed:taskify.iam.project.member.removed}") String iamProjectMemberRemovedQueue,
+            @Value("${rabbitmq.queue.iam-project-member-role-updated:taskify.iam.project.member.role.updated}") String iamProjectMemberRoleUpdatedQueue,
 
-            // Organization service queues
-            @Value("${rabbitmq.queue.org-user-created}") String orgUserCreatedQueue,
-            @Value("${rabbitmq.queue.org-user-deleted}") String orgUserDeletedQueue,
+            // Queue names - Organization service
+            @Value("${rabbitmq.queue.org-user-created:taskify.org.user.created}") String orgUserCreatedQueue,
+            @Value("${rabbitmq.queue.org-user-deleted:taskify.org.user.deleted}") String orgUserDeletedQueue,
 
-            // Project service queues
-            @Value("${rabbitmq.queue.project-user-created}") String projectUserCreatedQueue,
-            @Value("${rabbitmq.queue.project-user-deleted}") String projectUserDeletedQueue
+            // Queue names - Project service
+            @Value("${rabbitmq.queue.project-user-created:taskify.project.user.created}") String projectUserCreatedQueue,
+            @Value("${rabbitmq.queue.project-user-deleted:taskify.project.user.deleted}") String projectUserDeletedQueue
     ) {
-
         this.userEventsExchange = userEventsExchange;
         this.membershipEventsExchange = membershipEventsExchange;
         this.organizationEventsExchange = organizationEventsExchange;
